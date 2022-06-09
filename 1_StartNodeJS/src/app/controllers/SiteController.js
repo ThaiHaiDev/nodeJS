6ddb0f113afc919f4399f6baeac0ae18983d0956
Nguyễn Thái Hải <85157423+ThaiHaiDev@users.noreply.github.com>
2022-06-09 23:56:25 +0700

    model

--- 1_StartNodeJS/src/app/controllers/SiteController.js
+++ 1_StartNodeJS/src/app/controllers/SiteController.js
@@ -1,7 +1,19 @@
+const Course = require('../models/Course')
 class SiteController {
     // [GET] /
-    index(req, res) {
-        res.render('home')
+    index(req, res, next) {
+        // res.render('home')
+        // Course.find({}, function (err, courses) {
+        //     if (!err) {
+        //         res.json(courses)
+        //     } else {
+        //         res.status(400).json({ error: 'ERROR...'})
+        //     }
+        //   });
+
+        Course.find({})
+            .then(courses => res.json(courses))
+            .catch(next)
     }
 
     // [GET] /search
--- 1_StartNodeJS/src/index.js
+++ 1_StartNodeJS/src/index.js
@@ -8,22 +8,27 @@ const app = express()
 const port = 3000
 
 const route = require('./routes')
+const db = require('./config/db')  // Connect data
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
 app.use(morgan('combined'))
 
+// Connect Database
+db.connect()
+
+
 // Template engine
 app.engine('hbs', engine({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'))
+app.set('views', path.join(__dirname, 'resources', 'views'))
 
 // Routes init
 route(app)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
