961dff27b28a0ec6678d24a010711b0857774b6c
Nguyễn Thái Hải <85157423+ThaiHaiDev@users.noreply.github.com>
2022-09-20 20:57:24 +0700

    fix week 3

--- 0_NodeJS_Basic/Week_3/BT_W3/19110356.js
+++ 0_NodeJS_Basic/Week_3/BT_W3/19110356.js
@@ -5,10 +5,15 @@ const PORT = 5000;
 
 const server = http.createServer();
 
-const mygroup = [
+const teamGroup22 = [
     {id:19110356,name:'Nguyen Thai Hai'},
-    {id:19110462, name: 'Hoang Minh Thang'}
+    {id:19110462, name: 'Hoang Minh Thang'},
+    {id: 19110, name: 'Nguyen Huu Hieu'}
 ];
+
+const mygroup = [
+    {id:19110356,name:'Nguyen Thai Hai'}
+]
 
 server.on('request', (req, res) => {
     // req.url = /friend/0 => split('/') = ['', friend, '0']
@@ -19,17 +24,23 @@ server.on('request', (req, res) => {
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(mygroup))
             break;
-        case '22':
+        case '19110356':
             if (req.method == 'POST') {
                 req.on('data', (data) => {
                     const newFriend = data.toString();
                     if (mygroup.filter(e => e.id === JSON.parse(newFriend).id).length > 0) {
-                        console.log('Not valid')
+                        res.statusCode = 404;
+                        res.end('Not valid'); 
                     } else {
-                        mygroup.push(JSON.parse(newFriend));
+                        if (teamGroup22.filter(e => e.id === JSON.parse(newFriend).id).length > 0) {
+                            mygroup.push(JSON.parse(newFriend));
+                            req.pipe(res);
+                        } else {
+                            res.statusCode = 404;
+                            res.end('Not valid'); 
+                        }
                     }
                 })
-                req.pipe(res);
             } else {
                 const student = mygroup.filter(value => value.id === parseInt(items[2]))
                 res.statusCode = 200;
